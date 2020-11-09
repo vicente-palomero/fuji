58be948a4cd28d947d0f4c325b250bf8f657c5a7
Vicente Palomero <vicente.palomero.lopez@gmail.com>
2020-11-09 15:40:04 +0100

    Add to travis

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{vue}',
+    '!**/node_modules/**',
+    '!<rootDir>/dist/**',
+    '!<rootDir>/src/plugins/**',
+    '!<rootDir>/tests/unit/**'
+  ],
+  coverageReporters: [
+    'lcov',
+    'text-summary'
+  ]
 }
+
